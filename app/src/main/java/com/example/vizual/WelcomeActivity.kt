3890921751ea645076b4414de846be43f7ce26f9
rlram <rlramlakhan02@gmail.com>
2024-11-20 20:52:42 +0530

    add welcome screen

--- app/src/main/java/com/example/vizual/WelcomeActivity.kt
+++ app/src/main/java/com/example/vizual/WelcomeActivity.kt
@@ -1,36 +1,28 @@
 package com.example.vizual
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.animation.AnimationUtils
-import android.widget.TextView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-@SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_welcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val tvAppName: TextView = findViewById(R.id.tvAppName)
-        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide)
-        tvAppName.startAnimation(slideAnimation)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(applicationContext, WelcomeActivity::class.java)
+        val btnContinue: Button = findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener {
+            val intent = Intent(applicationContext, SignUpActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }
     }
 }
